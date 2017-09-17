a7b2ebbadce7c98a708a6a3d630c18ff3952bb39
neuecc <ils@neue.cc>
2017-09-17 18:58:50 +0900

    setup initial projs

---
+++ sandbox/ConsoleAppNet461/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppNet461
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ tests/Utf8Json.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Utf8Json.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
