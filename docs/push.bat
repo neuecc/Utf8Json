9eb17f7aab1ed5d4e48ecbe8db7afeec5207c616
neuecc <ils@neue.cc>
2017-10-14 02:39:36 +0900

    1.2.3

--- docs/push.bat
+++ docs/push.bat
@@ -1,4 +1,4 @@
-nuget push Utf8Json.1.2.2.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push Utf8Json.ImmutableCollection.1.2.2.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push Utf8Json.UnityShims.1.2.2.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push Utf8Json.AspNetCoreMvcFormatter.1.2.2.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push Utf8Json.1.2.3.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push Utf8Json.ImmutableCollection.1.2.3.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push Utf8Json.UnityShims.1.2.3.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push Utf8Json.AspNetCoreMvcFormatter.1.2.3.nupkg -Source https://www.nuget.org/api/v2/package
