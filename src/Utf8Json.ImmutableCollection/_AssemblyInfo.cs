cb48eb36b8408b6cb06383c8c0d76942c712b268
Yoshifumi Kawai <ils@neue.cc>
2018-01-23 18:37:39 +0900

    ready for 1.3.6.1

--- src/Utf8Json.ImmutableCollection/_AssemblyInfo.cs
+++ src/Utf8Json.ImmutableCollection/_AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 [assembly: Guid("2e7ac239-3db1-4772-9f47-5f62abb56ed0")]
-[assembly: AssemblyVersion("1.3.6")]
-[assembly: AssemblyFileVersion("1.3.6")]
+[assembly: AssemblyVersion("1.3.6.1")]
+[assembly: AssemblyFileVersion("1.3.6.1")]
