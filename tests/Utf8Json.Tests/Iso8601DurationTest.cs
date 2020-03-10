f80ffa84263710a559d146d6d749673a7180d319
Rex Ng <rexcfnghk@gmail.com>
2020-03-10 10:13:32 +0800

    Add tests

--- tests/Utf8Json.Tests/Iso8601DurationTest.cs
+++ tests/Utf8Json.Tests/Iso8601DurationTest.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Text;
 using Utf8Json.Formatters;
 using Utf8Json.Resolvers;
 using Xunit;
@@ -77,6 +79,76 @@ namespace Utf8Json.Tests
 
             // Assert
             Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(42)]
+        public void
+            Deserialize_ReturnsExpectedTimeSpan_WhenStringOnlyContainsPeriod(
+                int days)
+        {
+            // Arrange
+            var duration = $"{{ \"Test\": \"P{days}D\" }}";
+
+            var expected = new TimeSpan(days, 0, 0, 0);
+
+            // Act
+            var wrapper = JsonSerializer.Deserialize<TestWrapper>(duration);
+            var actual = wrapper.Test;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(42, 42, 42, 42)]
+        public void SerializeDeserialize_RoundTrips(
+            int days,
+            int hours,
+            int minutes,
+            int seconds)
+        {
+            // Arrange
+            var expected = new TimeSpan(days, hours, minutes, seconds);
+
+            // Act
+            using (var ms = new MemoryStream())
+            {
+                JsonSerializer.Serialize(ms, expected);
+                var actual = JsonSerializer.Deserialize<TimeSpan>(ms);
+
+                // Assert
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        [Theory]
+        [InlineData(42, 23, 42, 42)]
+        public void DeserializeSerialize_RoundTrips(
+            int days,
+            int hours,
+            int minutes,
+            int seconds)
+        {
+            // Arrange
+            var expected = $"\"P{days}DT{hours}H{minutes}M{seconds}S\"";
+            var wrappedString = $"{{ \"Test\": {expected} }}";
+
+            // Act
+            using (var ms = new MemoryStream())
+            using (var sw = new StreamWriter(ms))
+            {
+                sw.Write(wrappedString);
+                sw.Flush();
+                ms.Seek(0, SeekOrigin.Begin);
+
+                var deserialized = JsonSerializer.Deserialize<TestWrapper>(ms);
+                var serialized = JsonSerializer.Serialize(deserialized.Test);
+                var actual = Encoding.UTF8.GetString(serialized);
+
+                // Assert
+                Assert.Equal(expected, actual);
+            }
         }
 
         public class TestWrapper
