efdd1739d196b009ef628d81cefcc35977ef497d
Andrew Gumenyuk <dj_revo_1@mail.ru>
2020-04-18 22:49:47 +0300

    #118 Remove useless usings that were causing warnings

--- tests/Utf8Json.Tests/DataContractTest.cs
+++ tests/Utf8Json.Tests/DataContractTest.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Text;
 using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Utf8Json.Tests
